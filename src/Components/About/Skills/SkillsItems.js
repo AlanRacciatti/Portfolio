cfef0690cda367684decec0ca2a05351bba25933
AlanRacciatti <alanracciatti1220@gmail.com>
2022-03-08 17:16:19 -0300

    Add skills in about section

--- src/Components/About/Skills/SkillsItems.js
+++ src/Components/About/Skills/SkillsItems.js
@@ -1,13 +1,20 @@
+// FroEthers.jstend
 import htmlImg from '../../../assets/html-5.png'
 import cssImg from '../../../assets/css-3.png'
+import bootstrapImg from '../../../assets/bootstrap.jpg'
 import jsImg from '../../../assets/js.png'
 import reactImg from '../../../assets/physics.png'
+
+// Backend
 import nodeImg from '../../../assets/nodejs.png'
 import expressImg from '../../../assets/express.jpg'
 import mySqlImg from '../../../assets/mysql.png'
 import sequelizeImg from '../../../assets/images.png'
+
+// Blockchain
+import ethersJsImg from '../../../assets/ethersjs.svg'
 import solidityImg from '../../../assets/descarga (5).png'
-import bootstrapImg from '../../../assets/bootstrap.jpg'
+import hardhatImg from '../../../assets/hardhat.svg'
 
 const SkillsItems = [
     {
@@ -54,12 +61,25 @@ const SkillsItems = [
                 title: 'Sequelize',
                 image: sequelizeImg,
             },
+        ]
+    },
+    {
+        title: 'Blockchain',
+        items: [
+            {
+                title: 'Ethers.js',
+                image: ethersJsImg,
+            },
             {
                 title: 'Solidity',
                 image: solidityImg,
             },
+            {
+                title: 'Hardhat',
+                image: hardhatImg,
+            },
         ]
-    }
+    },
 ]
 
 export default SkillsItems
